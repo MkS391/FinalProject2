3a8a48fbf2d3430e219bae6acde569ca5bcadb3b
MkS391 <Mkrtich.s07@gmail.com>
2024-11-21 09:14:47 -0500

    Finished project 2
    Added the Average class
    AvergeDriver
    and completed the compactDisc file

--- Average.java
+++ Average.java
@@ -1,2 +1,50 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
 public class Average {
+    private int data[];
+    private double mean;
+
+    public Average() {
+        data = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < data.length; i++) {
+            System.out.print("Enter your score " + (i + 1) + ": ");
+            data[i] = sc.nextInt();
+        }
+        calculateMean();
+        selectionSort();
+    }
+
+    public void calculateMean() {
+        int total = 0;
+        for (int score : data) {
+            total += score;
+        }
+        mean = (double) total / data.length;
+    }
+
+    public String toString() {
+        String result = "Scores in descending order: ";
+        for (int i = 0; i < data.length; i++) {
+            result += data[i] + " ";
+        }
+        result += "\nMean: " + mean;
+        return result;
+    }
+
+    public void selectionSort() {
+        for (int i = 0; i < data.length - 1; i++) {
+            int MaxIndex = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] > data[MaxIndex]) {
+                    MaxIndex = j;
+                }
+            }
+            int temp = data[MaxIndex];
+            data[MaxIndex] = data[i];
+            data[i] = temp;
+        }
+    }
 }
